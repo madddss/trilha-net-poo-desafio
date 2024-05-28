f7b1bba6ce6c86d85e78dd40d3d509fd1b2975fd
Maria Eduarda Assumpção <119744320+madddss@users.noreply.github.com>
2024-05-28 09:29:59 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone ip = new Iphone("(43)-999321345", "Iphone 15", "34830922132", 128);
+ip.Ligar();
+ip.InstalarAplicativo("Facebook");
+ip.ReceberLigacao;
+
+Nokia nk = new Nokia("(43)-997439943", "Nokia AW", "90328110990", 32);
+nk.Ligar();
+ip.InstalarAplicativo("Jogo da Cobrinha");
+nk.ReceberLigacao();
